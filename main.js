24d47712d3d0af9f144b6b8e62fccd859de05bce
sou <sasaki.sou@samurai-adways.net>
2022-01-30 17:47:55 +0900

    POST player

--- main.js
+++ main.js
@@ -2,8 +2,25 @@ const express = require('express');
 
 const app = express();
 
+app.use(express.json());
+
+let players = [];
+
 app.get('/hello', (req, res) => {
     res.send("hello, world\n");
 });
+
+app.post('/players', (req, res) => {
+    const { name } = req.body;
+    if (players.some(p => p.name === name)) {
+        console.error("this name is already used: ", name);
+        res.status(409).send();
+        return;
+    }
+    players.push(req.body);
+    console.log("current players", players);
+
+    res.status(204).send();
+})
 
 app.listen(3000);
--- readme.md
+++ readme.md
@@ -1,9 +1,13 @@
 17:30 ~ 
 
 POST /players
+```
+curl -X POST localhost:3000/players -d '{ "name": "sasaki" }' -H 'Content-Type: application/json' --verbose
+```
 
 GET /players/1/games
 
 POST /players/1/declare
 
 bearer token on authentication header 
+
