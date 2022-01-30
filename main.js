d877b50438e0d88117000ce430ca09dc8099ecc1
sou <sasaki.sou@samurai-adways.net>
2022-01-30 23:12:50 +0900

    add questions init

--- main.js
+++ main.js
@@ -21,6 +21,27 @@ app.use(function (err, req, res, next) {
 
 const { v4 } = require('uuid');
 
+const QUESTION_TEXTS = [
+    ["where is same color", "同じ色が隣り合っている数字タイルはどこ？"],
+    ["where is 6 or 7", "6または7はどこ？（どちらかひとつ選ぶ）"],
+    ["how many reds", "赤の数字タイルは何枚ある？"],
+    ["sum", "[共有情報カード]数字タイル全ての数の合計は？"],
+    ["where is 1 or 2", "1または2はどこ？（どちらかひとつ選ぶ）"],
+    ["how many evens", "偶数は何枚ある？（0も含む）"],
+    ["big 3 sum", "大きい方から3枚の数の合計は？"],
+    ["continuous positions", "数が連続している数字タイルはどこ？"],
+    ["where is 0", "0はどこ？"],
+    ["where is 3 or 4", "3または4はどこ？（どちらかひとつ選ぶ）"],
+    ["where is 8 or 9", "8または9はどこ？（どちらか一つ選ぶ）"],
+    ["how many odds", "奇数は何枚ある？"],
+    ["same number pairs", "同じ数字タイルのペアは何組ある？"],
+    ["how many blues", "青の数字タイルは何枚ある？"],
+    ["reds sum", "赤の数の合計は？"],
+    ["where is 5", "5はどこ？"],
+    ["small three sum", "小さい方から3枚の数の合計は？"],
+    ["blues sum", "青の数の合計は？"],
+    ["big - small", "[共有情報カード]数字タイルの最大の数から、最小の数を引いた数は？"],
+];
 
 let players = [];
 let answerCards = [];
@@ -88,7 +109,15 @@ app.post('/init', (req, res) => {
     console.log("All cards were passed out", players.map(p => JSON.stringify(p)));
     answerCards = cards;
 
-    res.send(answerCards);
+    questions = shuffle(QUESTION_TEXTS).map(([_, text], i) => {
+        return {
+            id: i + 1,
+            text,
+            opens: i < 5
+        }
+    });
+
+    res.send(questions);
 })
 
 // app.get('/game', (req, res) => {
